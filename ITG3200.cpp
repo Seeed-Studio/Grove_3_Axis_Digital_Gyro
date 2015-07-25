f4fd91cb9bba7c9d4096715c726c8bae823d0c11
Jack Shao <jacky.shaoxg@gmail.com>
2015-07-25 17:41:11 +0800

    Update ITG3200.cpp
    Fixed a bug in zeroCalibratiion within Arduino 1.6.4+ env.

--- ITG3200.cpp
+++ ITG3200.cpp
@@ -117,9 +117,9 @@ void ITG3200::getAngularVelocity(float *ax,float *ay,float *az)
 }
 void ITG3200::zeroCalibrate(unsigned int samples, unsigned int sampleDelayMS) 
 {
-  int16_t x_offset_temp;
-  int16_t y_offset_temp;
-  int16_t z_offset_temp;
+  int16_t x_offset_temp = 0;
+  int16_t y_offset_temp = 0;
+  int16_t z_offset_temp = 0;
   int16_t x,y,z;
   x_offset = 0;
   y_offset = 0;
